a53113833312e5232d575135fc30614ea5dde829
coreygumbs@gmail.com <cgumbs@fullsail.edu>
2014-07-14 18:42:16 -0400

    Update to defining and defining variables

--- practice for class and text book/videos from class/js/variables.js
+++ practice for class and text book/videos from class/js/variables.js
@@ -1,3 +1,8 @@
 //JavaScript
 
-var age;
+//declaring and defining variables
+var age; //declaration
+age = 37; //definition
+//storage container = value
+
+var yearOfBirth = 1977; // you can declare and define on the same line

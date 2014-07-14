d758749c59e39347e6104d44b8945416e81f6066
coreygumbs@gmail.com <cgumbs@fullsail.edu>
2014-07-14 19:27:03 -0400

    boolean, declaring, and defning variables

--- practice for class and text book/videos from class/js/variables.js
+++ practice for class and text book/videos from class/js/variables.js
@@ -6,3 +6,9 @@ age = 37; //definition
 //storage container = value
 
 var yearOfBirth = 1977; // you can declare and define on the same line
+
+//creating boolean variables
+
+var isStudent = false;//boolean variable
+
+var firstName= "Corey";

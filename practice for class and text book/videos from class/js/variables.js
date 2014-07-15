d79e7356bd9d34cacf8f870e83e60deb73aaebcc
coreygumbs@gmail.com <cgumbs@fullsail.edu>
2014-07-14 20:01:02 -0400

    constructing arrays practice
    went over arrays, variables, array axis notation

--- practice for class and text book/videos from class/js/variables.js
+++ practice for class and text book/videos from class/js/variables.js
@@ -11,4 +11,16 @@ var yearOfBirth = 1977; // you can declare and define on the same line
 
 var isStudent = false;//boolean variable
 
-var firstName= "Corey";
+var firstName= "Corey";//string
+
+
+//creating arrays
+var muppetNames = ["Kermit", "Miss Piggy", "Gonzo", "Rizzo"];
+
+//Array Axis Notation: each item has an index number. first input has an index number of "0" then "1", "2", etc.
+muppetNames[3] = "Fonzie";  //change input of index number
+muppetNames[1] = "Animal";
+
+var num = 3;// will still display index #3 which is Fonzie
+
+console.log(muppetNames[num]);

4bc40c3d1736df0bd768f1930e5afbf9f8fde418
coreygumbs@gmail.com <cgumbs@fullsail.edu>
2014-07-15 21:17:55 -0400

    Problem # 3 - Slice of Pizza part 2
    code and expression for problem number 3

--- Expressions_worksheet/js/worksheet.js
+++ Expressions_worksheet/js/worksheet.js
@@ -2,7 +2,7 @@
 //JavaScript
 
 
-//Begining of Problem #1
+//Begining of Problem #1 - Dog Years
 
 //declaring and defining variables sparkyAge
 var sparkyAge = 2;
@@ -15,10 +15,10 @@ console.log("Sparky is" + " " + sparkyAge + " " + "human years old, which is" + 
 
 
 
-//Begining of Problem # 2
+//Begining of Problem # 2 - Slice of Pie Part 1
 
 //decalring and defining variables for how many slices per pizza, how many people at party, how many pizzas ordered.
-var pizzaSlice = 12;
+var pizzaSlice = 10;
 var partyPeople = 50;
 var pizzaOrdered = 26;
 
@@ -27,3 +27,17 @@ var slicesPeople = Number(pizzaSlice * pizzaOrdered)/partyPeople;
 
 //print out of expresssion for Problem #2
 console.log("Each person ate" + " " + slicesPeople + " " + "slices of pizza at the party.");
+
+
+
+//Begining of Problem #3 - Slice of Pie part 2
+
+
+//used variables from Problem #2 
+var pizzaSlice = 10;
+var partyPeople = 50;
+var pizzaOrdered = 26;
+
+//calculate the number of pizzas, slices, people, and remainder to sparky
+var resultSparky = Number(pizzaSlice * pizzaOrdered) / partyPeople;
+console.log("Sparky got" + " " +resultSparky + " " + "slice of pizza.");

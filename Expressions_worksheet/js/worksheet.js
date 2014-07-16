da52ac95ae69fa9168075be6ff43185c6772f96e
coreygumbs@gmail.com <cgumbs@fullsail.edu>
2014-07-15 20:41:15 -0400

    Activity: Expression Worksheet Problem # 2
    Completed Problem #2

--- Expressions_worksheet/js/worksheet.js
+++ Expressions_worksheet/js/worksheet.js
@@ -12,3 +12,18 @@ var result = sparkyAge * 7;
 
 //result to print -  created concatenating strings to print result and sparkyAge expression
 console.log("Sparky is" + " " + sparkyAge + " " + "human years old, which is" + " " + result + " " + "in dog years." );
+
+
+
+//Begining of Problem # 2
+
+//decalring and defining variables for how many slices per pizza, how many people at party, how many pizzas ordered.
+var pizzaSlice = 12;
+var partyPeople = 50;
+var pizzaOrdered = 26;
+
+//multiply pizza slices and pizza ordered, divide by # of people at party & assign result var
+var slicesPeople = Number(pizzaSlice * pizzaOrdered)/partyPeople;
+
+//print out of expresssion for Problem #2
+console.log("Each person ate" + " " + slicesPeople + " " + "slices of pizza at the party.");

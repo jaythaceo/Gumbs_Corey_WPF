853c083cf53ffc40901292b0abd7a867e5763579
coreygumbs@gmail.com <cgumbs@fullsail.edu>
2014-07-15 20:25:15 -0400

    Activity: Expression Worksheet Problem #1
    completion of problem #1 with comments

--- Expressions_worksheet/js/worksheet.js
+++ Expressions_worksheet/js/worksheet.js
@@ -5,8 +5,10 @@
 //Begining of Problem #1
 
 //declaring and defining variables sparkyAge
-
 var sparkyAge = 2;
+
+//result variable created
 var result = sparkyAge * 7;
 
-console.log("Sparky is" + " " + sparkyAge + " " + "which is " );
+//result to print -  created concatenating strings to print result and sparkyAge expression
+console.log("Sparky is" + " " + sparkyAge + " " + "human years old, which is" + " " + result + " " + "in dog years." );

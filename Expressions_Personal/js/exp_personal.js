9632ce39100edad5f4e1c91f9d2bb5c1ea339a4f
coreygumbs@gmail.com <cgumbs@fullsail.edu>
2014-07-16 21:11:06 -0400

    Correction to total formula

--- Expressions_Personal/js/exp_personal.js
+++ Expressions_Personal/js/exp_personal.js
@@ -11,26 +11,24 @@
 //title alert
 alert("RecCalc will determine your free time for the week! \n \n Please answer all questions. Click next to begin.");
 //input variables
-var weekTime = 168;//hours in a week
+var weekTime = 168 - total;//hours in a week minus hours spent = free time
 var userSchool = prompt("How many hours a week do you spend on school work?");
 var userStudy = prompt("How many hours a week do you study?");
 var userSleep = prompt("How many hours do you sleep in a day?")
 var userEat = prompt("How many times do you eat per day?")
-var total= Number(userSchool + userStudy + userSleep + userEat);
+var total = userSchool + userStudy + Sleep + eat;
 
 
 var eat =(userEat * 30 / 60)* 7;//hours per week for eat: convert minutes into hours per week
-var sleep = (userSleep * 7)//hours per tingweek for sleep
-var avgStudy = Number(userStudy)/7;
-var result = Number(total - weekTime);
+var sleep = (userSleep * 7)//hours per week for sleep
+var avgStudy = Number(userStudy)/7;//average per day of study time
 
+//result alerts
 alert("You do " + userSchool + " hours worth of school work.");
 alert("You study for " + userStudy + " hours a week, for an average of " + avgStudy + " hours per day.");
 alert("You sleep a total of " + sleep + " hours per week." );
-alert(eat);
-alert(result);
+alert("You eat a total of " + eat + " hours per week.");
+alert(weekTime);
 
 
- 
-console.log(userSchool + " " + userStudy + " " + userSleep + " " + userEat);
-console.log(eat + " " + sleep);
+console.log(total);
